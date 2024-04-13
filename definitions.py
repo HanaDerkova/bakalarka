e8c9a139d3720242c95288e79f773a6c78585fcb
HanaDerkova <hankaderkova@gmail.com>
2024-04-13 16:56:15 +0200

    added additionnal arguments

---
+++ definitions.py
@@ -0,0 +1,315 @@
+import numpy as np
+from scipy.optimize import Bounds, minimize
+from scipy.stats import norm, entropy
+
+# convention the escape state is always the last one
+# should makesense in long run
+# not gonna use softmax for params so far cause only 2 edges do not need it
+# chain -> params the slucka pravdepodobnost on each state
+# escape_chain -> params probability of escaping from each state
+# combined -> self loop = 1, transition ,
+def generate_matrix(parameters, architecture, number_of_states):
+    if architecture == "chain":
+        array = np.zeros((number_of_states,number_of_states))
+        for i in range(number_of_states - 1):
+            x = parameters[i]
+            prob_of_self_looping = np.exp(x) / (np.exp(x) + np.exp(0))
+            array[i][i] = prob_of_self_looping # set the diagonal whis is slucka to param[i]
+            array[i][i + 1] = np.exp(0) / (np.exp(x) + np.exp(0))  # set the transition from i to i+1
+        #array[number_of_states][number_of_states] = 0 # from escape state we aint gonna go anywhere
+        matrix = array
+    elif architecture == "escape_chain":
+        matrix = np.zeros((number_of_states,number_of_states))
+        for i in range(number_of_states - 1):
+            x = parameters[i]
+            prob_of_self_looping = np.exp(x) / (np.exp(x) + np.exp(0))
+            matrix[i][number_of_states - 1] = prob_of_self_looping
+            matrix[i][i + 1] = np.exp(0) / (np.exp(x) + np.exp(0))
+    elif architecture == "combined":
+        matrix = np.zeros((number_of_states,number_of_states))
+        for i in range(number_of_states - 1):
+            transition_prob_parameter = parameters[2*i]
+            escaping_prob_parameter = parameters[2*i + 1]
+            self_looping_parameter = 0
+            sum = np.exp(transition_prob_parameter) + np.exp(escaping_prob_parameter) + np.exp(self_looping_parameter)
+            prob_self_looping = np.exp(self_looping_parameter) / sum
+            prob_trasnition = np.exp(transition_prob_parameter) / sum
+            prob_escaping = np.exp(escaping_prob_parameter) / sum
+            matrix[i][i] = prob_self_looping
+            matrix[i][number_of_states - 1] = prob_escaping
+            matrix[i][i + 1] = prob_trasnition
+    elif architecture == "full":
+        matrix = generate_full_mch(parameters=parameters, number_of_states=number_of_states)
+    else:
+        raise ValueError("Invalid architecture")
+
+    return matrix
+
+# using this in calculations (1 0 0 0 0 0 0) ,for the starting state
+def create_vector(number_of_states):
+    vec = np.zeros(number_of_states)  # Create an array of zeros
+    vec[0] = 1  # Set the 0th element to 1
+    return vec
+
+# this has to be specific for architecture
+def mch_to_likelyhood_old(parameters, data ,architecture, number_of_states):
+    likelyhoods = [0]
+    matrix = np.array( generate_matrix(parameters, architecture, number_of_states) )
+    #print(matrix)
+    vector = np.array( create_vector(number_of_states) )
+    result = vector[:] # we didd cpy here
+    data_likelyhoods = []
+    max_dist = max(data)
+    for i in range(max_dist):
+        result = np.dot(result, matrix) # zavinac
+        likelyhoods.append(result[-1])  # [-1] gets u the last elem, likelyhoods[0] = 0
+    for distance in data:
+        data_likelyhoods.append(likelyhoods[distance])  # here ich mas pomiesane, nemalo by na tom zalezat, sum is komunitative
+    return likelyhoods, data_likelyhoods
+
+def precompute_powers(matrix, max_exponent):
+    powers = [matrix]
+    current_power = matrix
+    for i in range(max_exponent.bit_length() - 1):
+        current_power = np.dot(current_power, current_power)
+        powers.append(current_power)
+    return powers
+
+def matrix_power(matrix, exponent, precomputed_powers):
+    binary_exponent = bin(exponent)[2:][::-1]
+    result = np.eye(matrix.shape[0])
+    for i, bit in enumerate(binary_exponent):
+        if bit == '1':
+            result = np.dot(result, precomputed_powers[i])
+    return result
+
+# this has to be specific for architecture
+def mch_to_likelyhood(parameters, data ,architecture, number_of_states):
+    matrix = np.array( generate_matrix(parameters, architecture, number_of_states) )
+    #print(matrix)
+    vector = np.array( create_vector(number_of_states) )
+    vector = vector[:] # we didd cpy here
+    data_likelyhoods = []
+    max_dist = max(data)
+    precomputed_powers = precompute_powers(matrix, int(max_dist))
+    unique_data_likelyhoods = {}
+    for distance in np.unique(data):
+      result = matrix_power(matrix, distance, precomputed_powers)
+      prob_of_apsorption = (vector @ result)[-1]
+      unique_data_likelyhoods[distance] = prob_of_apsorption
+    for distance in data:
+      # what herre???
+      data_likelyhoods.append(unique_data_likelyhoods.get(distance))  # here ich mas pomiesane, nemalo by na tom zalezat, sum is komunitative
+    return data_likelyhoods
+
+# eps <- small constant to ensure that we do not log(0)
+def objective_function(parameters, data ,architecture, number_of_states, eps=1e-20):
+    data_likelyhoods = mch_to_likelyhood(parameters, data ,architecture,number_of_states)
+    negative_log_likelihood = -np.sum(np.log(np.array(data_likelyhoods) + eps)) / len(data)
+    return negative_log_likelihood
+
+def extract_feature_lengths(bed_file):
+    feature_lengths = []  # Array to store feature lengths
+
+    # Open the BED file
+    with open(bed_file, 'r') as file:
+        # Iterate through each line in the file
+        for line in file:
+            # Split the line into columns based on tab delimiter
+            columns = line.strip().split('\t')
+            if len(columns) < 3:
+              continue
+            #print(columns)
+            # Extract start and end positions from the appropriate columns
+            start_position = int(columns[1])
+            end_position = int(columns[2])
+
+            # Calculate feature length and append to the array
+            feature_length = end_position - start_position
+            feature_lengths.append(feature_length)
+
+    return feature_lengths
+
+def create_parameters(input, number_of_states):
+  vector = np.zeros((number_of_states -1) * 2)
+  for i in range(number_of_states -1):
+    vector[2*i] = input # transition
+    vector[2*i + 1] = -5 #escaping je neparne probability, toto chceme nizke
+  return vector
+
+def compute(input, data, number_of_states, architecture):
+  parameters = create_parameters(input, number_of_states)
+  return objective_function(parameters, data ,architecture, number_of_states)
+
+def expected_number_of_steps(t, Q):
+  identity_matrix = np.eye(t)
+  N = np.linalg.inv(identity_matrix - Q)
+  column_vector = np.ones((t, 1))
+  return (N @ column_vector)
+
+def compute_mean(P):
+  Q = P[:-1, :-1]
+  t,z = Q.shape
+  return expected_number_of_steps(t,Q)[0,0]
+
+def variance_number_of_steps(P):
+  Q = Q = P[:-1, :-1]
+  t,z = Q.shape
+  identity_t = np.eye(t)
+  N = np.linalg.inv(identity_t - Q)
+  exp_number_of_steps = expected_number_of_steps(t,Q)
+  # Compute the Hadamard product of t with itself
+  tsq = np.square(exp_number_of_steps)
+  return (( 2 * N - identity_t) @ exp_number_of_steps - tsq)[0][0]
+
+def objective_function_2(parameters, data, architecture, number_of_states):
+  # Calculate the mean
+  mean_data = np.mean(data)
+
+  # Calculate the variance
+  #variance_data = np.var(data)
+
+  P = generate_matrix(parameters, architecture, number_of_states)
+  mean_phase_type = compute_mean(P)
+
+  # # Calculate the variance
+  # variance_phase_type = variance_number_of_steps(P)
+
+  return (abs(mean_data - mean_phase_type))*100
+
+def calculate_gap_lengths(bed_file):
+    # Initialize variables
+    last_end = 0
+    last_chromosome = None
+    gap_lengths = []
+
+    # Open and read the BED file
+    with open(bed_file, 'r') as f:
+        for line in f:
+            # Skip comment lines or empty lines
+            if line.startswith('#') or not line.strip():
+                continue
+            
+            # Parse BED interval
+            fields = line.strip().split('\t')
+            chromosome = fields[0]
+            start = int(fields[1])
+            end = int(fields[2])
+
+            # Check if chromosome changes
+            if chromosome != last_chromosome:
+                last_end = 0
+                last_chromosome = chromosome
+
+            # Check for gap
+            if start > last_end:
+                gap_lengths.append(start - last_end)
+            
+            # Update last_end
+            last_end = max(last_end, end)
+
+    # Check for gap after the last annotation
+    if last_end < end:
+        gap_lengths.append(end - last_end)
+
+    return gap_lengths
+
+def check_rows_sum_to_one(matrix):
+    row_sums = np.sum(matrix[:-1], axis=1)
+    return np.allclose(row_sums, 1)
+
+def generate_full_mch(parameters, number_of_states):
+    matrix = np.zeros((number_of_states, number_of_states))
+    counter = 0
+    for row in range(number_of_states - 1):
+        sum = 0
+        matrix_row = np.zeros(number_of_states)
+        for column in range(number_of_states):
+            parameter = np.exp(parameters[counter])
+            sum += parameter
+            matrix_row[column] = parameter
+            counter += 1
+        matrix[row] = matrix_row / sum
+    if (check_rows_sum_to_one(matrix)) :
+        return matrix
+    else:
+        print("smthing went wrong")
+
+def optimize_once(args_list):
+  number_of_states, data, bounds, options, architecture = args_list
+  if architecture == 'full':
+      initial_guess = np.random.rand((number_of_states - 1) * number_of_states)
+  elif architecture == 'combined': 
+      initial_guess = np.random.rand((number_of_states - 1) * 2)
+  elif architecture == "chain" or architecture == 'escape_chain':
+      initial_guess = np.random.rand(number_of_states - 1)
+  optimization_results = minimize(objective_function, initial_guess, method='L-BFGS-B', args=(data, architecture, number_of_states), bounds=bounds, options=options)
+  return optimization_results
+
+def opt_w_initialization(args_list):
+  number_of_states, data, bounds, options, initial_guess, architecture = args_list
+  optimization_results = minimize(objective_function, initial_guess, method='L-BFGS-B', args=(data, architecture, number_of_states), bounds=bounds, options=options)
+  return optimization_results
+
+def preprocessinig(args_list):
+  number_of_states, data, bounds, options, architecture = args_list
+  if architecture == 'full':
+      initial_guess = np.random.rand((number_of_states - 1) * number_of_states)
+  elif architecture == 'combined': 
+      initial_guess = np.random.rand((number_of_states - 1) * 2)
+  elif architecture == "chain" or architecture == 'escape_chain':
+      initial_guess = np.random.rand(number_of_states - 1)
+  optimization_results = minimize(objective_function_2, initial_guess, method='L-BFGS-B', args=(data, architecture, number_of_states), bounds=bounds, options=options)
+  return optimization_results
+
+def sh_entropy(data):
+    unique_values, value_counts = np.unique(data, return_counts=True)
+    probabilities = value_counts / len(data)
+    entropy_value = entropy(probabilities)
+    return entropy_value
+
+def max_mean_bounds(number_of_states, architecture, lower, upper):
+    if architecture == "chain" or architecture == "escape_chain":
+        params = np.full((number_of_states - 1), upper)
+        P = generate_matrix(params, architecture, number_of_states)
+    elif architecture == "combined":
+        params = np.full((number_of_states - 1) * 2, lower)
+        P = generate_matrix(params, architecture, number_of_states)
+    elif architecture == "full":
+        pass
+    else:
+        print("Unkonwn architecture")
+    
+    return compute_mean(P)
+
+def pruning(matrix, threshold):
+    new_matrix = np.copy(matrix)
+    
+    # Apply threshold condition for absolute values
+    mask = np.abs(new_matrix) < threshold
+    new_matrix[mask] = 0
+    
+    # Normalize each row to ensure the sum of weights in each row is 1
+    row_sums = new_matrix.sum(axis=1)
+    row_sums[row_sums == 0] = 1  # Avoid division by zero
+    new_matrix /= row_sums[:, np.newaxis]
+
+    return new_matrix
+
+def matrix_to_likelyhood(matrix, data, number_of_states):
+    likelyhoods = [0]
+    vector = np.array( create_vector(number_of_states) )
+    result = vector[:] # we didd cpy here
+    data_likelyhoods = []
+    max_dist = max(data)
+    for i in range(max_dist):
+        result = np.dot(result, matrix) # zavinac
+        likelyhoods.append(result[-1])  # [-1] gets u the last elem, likelyhoods[0] = 0
+    for distance in data:
+        data_likelyhoods.append(likelyhoods[distance])  # here ich mas pomiesane, nemalo by na tom zalezat, sum is komunitative
+    return likelyhoods, data_likelyhoods
+
+def obj_func_matrix(matrix, data, number_of_states, eps=1e-20):
+    lilelyhoods, data_likelyhoods = matrix_to_likelyhood(matrix, data,number_of_states)
+    negative_log_likelihood = -np.sum(np.log(np.array(data_likelyhoods) + eps)) / len(data)
+    return negative_log_likelihood
--- train_model.py
+++ train_model.py
@@ -1,13 +1,150 @@
 import argparse
+import matplotlib.pyplot as plt
+from definitions import extract_feature_lengths, calculate_gap_lengths, preprocessinig, opt_w_initialization, mch_to_likelyhood_old, sh_entropy
+from multiprocessing import Pool
+import numpy as np
+from scipy.stats import norm
+import os
+import sys
 
 parser = argparse.ArgumentParser(description="parse arguments for training")
 parser.add_argument('input_file', type=str, help='path to input bed file')
 parser.add_argument('-o', dest='output_dir', type=str, help='path to output directory')
 parser.add_argument('-architecture', type=str ,help='specify architecture for training')
-parser.add_argument('--number_of_states','-ns' type=int, help='Number of states for Markov chain')
-parser.add_argument('--opt', default=None , help='options for optimizer')
+parser.add_argument('-number_of_states','-ns', type=int, help='Number of states for Markov chain')
+# parser.add_argument('--options','-opt', default=None , help='options for optimizer')
 parser.add_argument('--threads', '-tr ',default=1, type=int, help='number of threads for optimization')
+parser.add_argument('--fit', type=str, default='i', choices=['g', 'd', 'i'], help='fit gaps / intervals or load distribution from .npy file, defalut intervals')
+parser.add_argument('--percent_visualize', '-p_v', defalut=100 ,type=int, help='percent of the data to be visualized, when hadling large bed file')
+parser.add_argument('--sample', '-s', type=int, defalut = None, hep='sample to preform training on, if bed file is too big to speed up training')
+
+# Add optional parameters for L-BFGS-B optimization
+parser.add_argument('--ftol', type=float, default=1e-5, help='tolerance for convergence')
+parser.add_argument('--gtol', type=float, default=1e-5, help='gradient norm tolerance')
+parser.add_argument('--max_iter', type=int, default=15000, help='maximum number of iterations')
+parser.add_argument('--max_cor', type=int, default=10, help='maximum number of corrections')
+
+
 
 args = parser.parse_args()
 
 print(args)
+
+if args.threads < 1:
+    sys.exit("Error: Number of threads must be at least 1.")
+
+if args.percent_visualize < 0:
+    sys.exit("Error: Percent to visualize cannot be negative.")
+
+
+if args.fit == 'g' :
+    data = calculate_gap_lengths(args.input_file)
+elif args.fin == 'i':
+    data = extract_feature_lengths(args.input_file)
+elif args.fit == "d":
+    data = np.load(args.input_file)
+
+
+if args.architecture == "full" :
+    bounds = [(-15, 15) for _ in range((args.number_of_states - 1) * (args.number_of_states))]
+elif args.architecture == "combined":
+    bounds = [(-15, 15) for _ in range((args.number_of_states - 1) * 2 )]
+elif args.architecture == "chain" or args.architecture == "escape_chain" :
+    bounds = [(-2, 2) for _ in range(args.number_of_states - 1)]
+
+options = {'maxfun': 60000,
+     'ftol': args.ftol,
+     'gtol': args.gtol,
+     'max_iter': args.max_iter,
+     'max_cor': args.max_cor}  # Add optional parameters to the options dictionary
+
+
+args_list = [(args.number_of_states, data, bounds, options, args.architecture )] * args.threads
+
+with Pool(args.threads) as pool:
+    # Perform optimization in parallel
+    results = pool.map(preprocessinig, args_list)
+
+#choose the most optimal initalization value
+best_initalization = None
+best_fun = float('inf')  # Initialize to positive infinity
+
+# Iterate through all optimization results
+for i, result in enumerate(results):
+    # Check if the current result has a smaller fun value than the current best
+    if result.fun < best_fun:
+        best_initalization = result
+        best_fun = result.fun
+num_additional_initializations = args.threads - 1
+
+# Sigma for Gaussian noise
+sigma = 0.1
+
+# Create additional initializations
+initial_guesses = [best_initalization.x]  # Initialize with the best_initialization
+for i in range(num_additional_initializations):
+    # Generate Gaussian noise
+    noise = np.random.normal(loc=0, scale=sigma, size=len(best_initalization.x))
+
+    # Add noise to the best_initialization
+    initialization = best_initalization.x + noise
+
+    # Append the new initialization to the list
+    initial_guesses.append(initialization)
+
+args_list = [(args.number_of_states, data, bounds, options, i , args.architecture) for i in initial_guesses]
+
+with Pool(args.threads) as pool:
+    results = pool.map(opt_w_initialization, args_list)
+    
+best_result = None
+best_fun = float('inf')  # Initialize to positive infinity
+
+# Iterate through all optimization results
+for i, result in enumerate(results):
+    # Check if the current result has a smaller fun value than the current best
+    if result.fun < best_fun:
+        best_result = result
+        best_fun = result.fun
+
+# Print the best optimization result
+if best_result:
+    print(f"Best Optimization Result: {best_result}")
+else:
+    print("No optimization results found.")
+
+
+vizualize_data = data 
+if args.percent_visualize < 100:
+    data_percentile = np.percentile(data, args.percent_visualize)
+    vizualize_data = data[data <= data_percentile]
+
+os.makedirs(args.output_dir, exist_ok=True)
+
+plt.hist(vizualize_data, density=True)
+likelyhoods, data_likelyhoods = mch_to_likelyhood_old(best_result.x , data ,args.architecture, args.number_of_states)
+s_e = sh_entropy(data)
+plt.plot(likelyhoods, label=f"states {args.number_of_states} cross entropy: {best_result.fun:.2f} sh entropy: {s_e:.2f}")
+plt.legend()
+plt.savefig(os.path.join(args.output_dir, "data_vs_training.svg"))
+plt.close()
+
+parameters = best_result.x
+cross_entropy = best_result.fun
+
+# Save parameters to a text file
+with open(os.path.join(args.output_dir, "trained_model.txt"), "a+") as file:
+    file.write(f"Architecture: {args.architecture}\n")
+    file.write(f"Number of States: {args.number_of_states}\n")
+    file.write("Parameters:\n")
+    for param in parameters:
+        file.write(f"{param}\n")
+
+np.save(os.path.join(args.output_dir, "cross_entropy.npy"), parameters)
+
+
+
+
+
+
+
